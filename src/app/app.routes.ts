58b9f1defa40c7b504a2f7d87ce9aedcbfd9f8d4
Dustin <d.dan342@gmail.com>
2024-06-10 23:18:30 -0700

    remove unused routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,10 @@
 import { Routes } from '@angular/router';
-import { authGuard } from '@lib/guards';
 
 export const routes: Routes = [
     {
-        path: 'auth',
-        loadChildren: async () => (await import('@pages/auth')).routes,
-        canMatch: [authGuard({ requiresAuthentication: false })],
-    },
-    {
         path: '',
         loadChildren: async () => (await import('@pages/home')).routes,
-        canMatch: [authGuard()],
-    },
-    {
-        path: 'users/:username',
-        loadChildren: async () => (await import('@pages/user')).routes,
-        canMatch: [authGuard()],
-    },
-    {
-        path: 'settings',
-        loadChildren: async () => (await import('@pages/settings')).routes,
-        canMatch: [authGuard()],
+        canMatch: [],
     },
     {
         path: '**',
