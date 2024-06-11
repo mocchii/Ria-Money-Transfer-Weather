859b1cdbe84cffe6b12b8c9a92d37aa34212966e
Dustin <d.dan342@gmail.com>
2024-06-10 23:29:57 -0700

    alter around

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@lib/guards';
 
 export const routes: Routes = [
     {
+        path: 'auth',
+        loadChildren: async () => (await import('@pages/auth')).routes,
+        canMatch: [authGuard({ requiresAuthentication: false })],
+    },
+    {
         path: '',
         loadChildren: async () => (await import('@pages/home')).routes,
-        canMatch: [],
+        canMatch: [authGuard()],
     },
     {
         path: '**',
